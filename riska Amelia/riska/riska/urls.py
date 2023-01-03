71a115044095fb647e4af5a754cc687b334c7254
riskaameliaputri <107563579+riskaameliaputri@users.noreply.github.com>
2023-01-03 21:47:45 +0700

    Add files via upload

---
+++ riska Amelia/riska/riska/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from blog.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index),
+    path('blog/', blogs),
+    path('tambah/', Data_baru, name='data_baru'),
+    path('blogsdetail/<int:id>', blogsdetail, name='detail'),
+]
